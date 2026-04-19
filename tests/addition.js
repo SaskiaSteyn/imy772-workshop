d01af7fd2195e134173809f8973047e3aa6842fe
JohanJvR <u22590732@tuks.co.za>
2026-04-19 17:09:14 +0200

    Addition

---
+++ tests/addition.js
@@ -0,0 +1,10 @@
+function addition(a, b) {
+    const decimalA = parseInt(a, 16);
+    const decimalB = parseInt(b, 16);
+
+    const result = decimalA + decimalB;
+
+    return result.toString(16).toUpperCase().padStart(4, '0');
+}
+
+module.exports = addition;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,1 +1,2 @@
 require('./sum-test.js');
+require('./addition.js');
