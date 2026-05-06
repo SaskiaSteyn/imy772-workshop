7ea65dedcd7cad266e7b6a51fab5ad4faab2d0e6
Saskia Steyn <u17267162@tuks.co.za>
2026-05-06 16:44:21 +0200

    fix: commented all of the tests back in

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,7 @@
-// require('./addition-test.js');
-// require('./subtraction-test.js')
-// require('./multiplication-test.js')
-// require('./division-test.js')
+require('./addition-test.js')
+require('./subtraction-test.js')
+require('./multiplication-test.js')
+require('./division-test.js')
 
-// require('./validation-test.js')
+require('./validation-test.js')
 require('./hex-to-decimal-test.js')
